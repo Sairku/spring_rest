8a3f41e2077f5c1b67ef75aba2e0e5a34224d09a
Sairku <kukrias@gmail.com>
2024-12-17 22:47:26 +0200

    add code3

--- src/main/java/service/AccountService.java
+++ src/main/java/service/AccountService.java
@@ -4,7 +4,6 @@ import model.Account;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
-import reposirory.AccountRepository;
 
 import java.util.Optional;
 
--- src/main/java/service/CustomerService.java
+++ src/main/java/service/CustomerService.java
@@ -5,7 +5,6 @@ import model.Account;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
-import reposirory.CustomerRepository;
 import reposirory.AccountRepository;
 
 import java.util.List;
